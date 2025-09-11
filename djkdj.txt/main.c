c8cf966b8cacdb23ed86afd2a5cd23f83c8926c1
Harry <harrylee0415@gmail.com>
2025-09-11 14:32:06 +0800

    Add files via upload

---
+++ djkdj.txt/main.c
@@ -0,0 +1,23 @@
+/******************************************************************************
+
+                            Online C Compiler.
+                Code, Compile, Run and Debug C program online.
+Write your code in this editor and press "Run" button to compile and execute it.
+
+*******************************************************************************/
+
+#include <stdio.h>
+
+int main()
+{
+    printf("\t\t2025 9月\n");
+    printf("日\t一\t二\t三\t四\t五\t六\n");
+    printf("\033[1;34m31");
+    printf("\033[1;36m\t1\t2\t3\t4\t5\t6\n");
+    printf("7\t8\t9\t10\t11\t12\t13\n");
+    printf("14\t15\t16\t17\t18\t19\t20\n");
+    printf("21\t22\t23\t24\t25\t26\t27\n");
+    printf("28\t29\t30\t");
+    printf("\033[1;34m 1\t2\t3\t4\n");
+
+}
